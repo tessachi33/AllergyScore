9f47e079ab89b79df76d63258d91f0e4757b6d28
tessachi33 <tessachi33@yahoo.com>
2015-08-13 18:14:39 -0700

    set-up file paths and gradle.

---
+++ src/main/java/AllergyScore.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
+public class main vooid AllergyScore();{}
+   
